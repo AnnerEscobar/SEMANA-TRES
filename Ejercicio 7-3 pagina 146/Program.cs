5ce69f5023de8bc0856191806385865b2d3f65a3
ANNER RONALDO ESCOBAR CRUZ <anner123escobar@gmail.com>
2021-02-26 12:40:57 -0600

    subir nuevos archivos

---
+++ Ejercicio 7-3 pagina 146/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio_7_3_pagina_146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        }
+           public class Agenda
+        {
+            //DEclaramos las variables
+            private Contacto[] lista_contactos;
+            private int num_contactos;
+
+            //Metodo constructor
+            public Agenda()
+            {
+                lista_contactos = new Contacto[100];
+                num_contactos = 0;
+            }
+            //Metodo para a;adir un nuevo contacto a la agenda
+            //Entrada: nombre y telefono del contacto
+            //salida: void
+            //Nota: se permiten contactos repetidos
+
+            public void AnadirContacto(string nombre, int telefono)
+            {
+                Contacto C = new Contacto(nombre, telefono);
+                lista_contactos[num_contactos] = C;
+                num_contactos++;
+            }
+        }
+        }
+}
---
+++ Factorial de un numero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Factorial_de_un_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, factorial = 1;
+            Console.WriteLine("Ingresa el numero para saber su factorial");
+            numero = Int32.Parse(Console.ReadLine());
+
+            while(numero != 0)
+            {
+                factorial *= numero;
+                numero = numero -1;
+            }
+
+            Console.WriteLine("El factorial del numero que ingresaste es: {0} ", factorial);
+             
+        }
+    }
+}
