58ffa69b179deba99fa5c7849acf4e0708ddcb95
ANNER RONALDO ESCOBAR CRUZ <anner123escobar@gmail.com>
2021-02-25 23:24:24 -0600

    Subiendo primeras soluciones

---
+++ Ejercicio 5-5 pagina 110/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Tercera_semana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Dada una lista de 10 numeros reales, introduciods por el usuario, muestre los elementos menor
+            ///de dicha lista asi como la media aritmetica.
+
+
+            int n = 10;//declaracion de variables
+            float[] lista = new float[n];
+
+            Console.WriteLine();
+            Console.WriteLine("** EJERCICIO RESUELTO 5-5 ** ");
+            Console.WriteLine();
+
+            //Introducir los elementos de la lista
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write("Introduzcal el elemento {0} de la lista: ", i + 1);
+                float elem = float.Parse(Console.ReadLine());
+                lista[i] = elem;
+            }
+
+            //Mostrar la lista
+            Console.Write("\nLos elementos de la lista son:");
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(lista[i] + "\t");
+
+            //Calculo del menor, mayor y la media
+            float menor = lista[0];
+            float mayor = lista[0];
+            float media = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (menor > lista[i]) menor = lista[i];
+                if (mayor < lista[i]) mayor = lista[i];
+                media = (media + lista[i]); // suma los elementos
+            }
+
+            media = media / n;
+            // mostrar resultados
+            Console.WriteLine();
+            Console.WriteLine("El menor es: {0}", menor);
+            Console.WriteLine("El mayor es: {0}", mayor);
+            Console.WriteLine("La media es: {0}", media);
+
+        }
+    }
+}
---
+++ Ejercicio 5-6 pagina 110/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ejercicio_5_6_pagina_110
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            Console.WriteLine();
+            Console.WriteLine("** EJERCICIO RESUELTO 5-6 ** ");
+            Console.WriteLine();
+
+            Console.Write("Introduzca la dimension de la matriz: ");
+            int dim = Int32.Parse(Console.ReadLine());
+            int[,] matriz = new int[dim, dim];
+            for (i = 0; i < dim; i++)
+                for (j = 0; j < dim; j++)
+                    if (i == j)
+                        matriz[i, j] = 1;
+                    else
+                        matriz[i, j] = 0;
+
+            // Muestra la matriz
+            Console.WriteLine("La matriz id. de dimension {0} es :", dim);
+            for (i = 0; i < dim; i++)
+            {
+                for (j = 0; j < dim; j++)
+                    Console.Write(" {0,3}", matriz[i, j]);
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
